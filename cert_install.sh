165e5a487afa969c3d1e659998292660f2588ea2
Maxime Falaize <maxime.falaize@gmail.com>
2017-08-18 22:19:03 +0200

    Ajout test des arguments du script

--- cert_install.sh
+++ cert_install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 HAPROXY_CONFIG_PATH=/usr/local/etc/haproxy
 
+if [ -z ${1+x} ] || [ -z ${2+x} ] ; then
+    echo "Le script prend en paramètre le nom de domaine en première position et l'adresse mail en deuxième position"
+    exit 1
+fi
+
 docker exec -it haproxy test -f /etc/letsencrypt/live/$1/fullchain.pem || certbot certonly --standalone -n --agree-tos -m $2 -d $1 --preferred-challenges http --http-01-port 54321
 docker exec -it haproxy sed -i 's/#//' ${HAPROXY_CONFIG_PATH}/haproxy.cfg
 docker exec -it haproxy test -f ${HAPROXY_CONFIG_PATH}/fullchain.pem || ln -s /etc/letsencrypt/live/$1/fullchain.pem ${HAPROXY_CONFIG_PATH}/fullchain.pem
