4ae08b2e9586f63ce57d164e19ffa13824cd5b68
Maxime Falaize <maxime.falaize@gmail.com>
2017-08-18 22:11:36 +0200

    Correction erreur d'inattention

--- cert_install.sh
+++ cert_install.sh
@@ -3,7 +3,7 @@ HAPROXY_CONFIG_PATH=/usr/local/etc/haproxy
 
 docker exec -it haproxy test -f /etc/letsencrypt/live/$1/fullchain.pem || certbot certonly --standalone -n --agree-tos -m $2 -d $1 --preferred-challenges http --http-01-port 54321
 docker exec -it haproxy sed -i 's/#//' ${HAPROXY_CONFIG_PATH}/haproxy.cfg
-docker exec -it haproxy test -f ${HAPROXY_CONFIG_PATH}/fullchain.pem && ln -s /etc/letsencrypt/live/$1/fullchain.pem ${HAPROXY_CONFIG_PATH}/fullchain.pem
-docker exec -it haproxy test -f ${HAPROXY_CONFIG_PATH}/privkey.pem && ln -s /etc/letsencrypt/live/$1/privkey.pem ${HAPROXY_CONFIG_PATH}/privkey.pem
+docker exec -it haproxy test -f ${HAPROXY_CONFIG_PATH}/fullchain.pem || ln -s /etc/letsencrypt/live/$1/fullchain.pem ${HAPROXY_CONFIG_PATH}/fullchain.pem
+docker exec -it haproxy test -f ${HAPROXY_CONFIG_PATH}/privkey.pem || ln -s /etc/letsencrypt/live/$1/privkey.pem ${HAPROXY_CONFIG_PATH}/privkey.pem
 docker exec -it haproxy /etc/cron.daily/letsencrypt
 docker restart haproxy
