170fb460145db7bcb0373d4b7a469d421d9c55b9
Maxime Falaize <pro@maxime-falaize.fr>
2018-06-22 22:34:31 +0200

    Adaptation de la vue liste de l'administration des opérations

--- compta/admin.py
+++ compta/admin.py
@@ -3,9 +3,16 @@ from django.contrib import admin
 
 from compta.models import Compte, Categorie, Operation, Budget, CategorieEpargne, Epargne, OperationEpargne, Identifiant
 
+
+class OperationAdmin(admin.ModelAdmin):
+    list_display = ('date_operation', 'date_valeur', 'libelle', 'montant')
+    list_filter = ('date_operation', 'date_valeur')
+    search_fields = ('libelle',)
+
+
 admin.site.register(Compte)
 admin.site.register(Categorie)
-admin.site.register(Operation)
+admin.site.register(Operation, OperationAdmin)
 admin.site.register(Budget)
 admin.site.register(CategorieEpargne)
 admin.site.register(Epargne)
